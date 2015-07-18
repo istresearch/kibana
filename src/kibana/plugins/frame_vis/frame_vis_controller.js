0fe8671694072f4300722afb3d707955d862f94f
Marti Martinez <martinezah@gmail.com>
2015-07-18 08:10:16 +0000

    making initial trigger slightly more reliable

--- src/kibana/plugins/frame_vis/frame_vis_controller.js
+++ src/kibana/plugins/frame_vis/frame_vis_controller.js
@@ -6,22 +6,26 @@ define(function (require) {
       if (!url) return;
       var html = '<iframe data-id="' + $scope.$id + '" height="100%" width="100%" src="' + url + '"></iframe>';
       $scope.html = $sce.trustAsHtml(html);
-    });
-    $scope.$watch('esResponse', function (resp) {
       window.setTimeout(function () {
-        var frame = $('iframe[data-id="' + $scope.$id + '"]').get(0);
-        var query = $('form[name="queryInput"] input[type="text"]').val();
-        var filter = {
-          'query': query,
-          'time': window.$root.timefilter.time
-        };
-        frame.contentWindow.postMessage(JSON.stringify(filter), '*');
-      }, 100);
+        $scope.updateFrame();
+      }, 1000);
     });
     var $root = $scope;
     while ($root.$parent) {
       $root = $root.$parent;
     }
     window.$root = $root;
+    $scope.updateFrame = function () {
+      var frame = $('iframe[data-id="' + $scope.$id + '"]').get(0);
+      var query = $('form[name="queryInput"] input[type="text"]').val();
+      var filter = {
+        'query': query,
+        'time': window.$root.timefilter.time
+      };
+      frame.contentWindow.postMessage(JSON.stringify(filter), '*');
+    };
+    $scope.$watch('esResponse', function (resp) {
+      $scope.updateFrame();
+    });
   });
 });
